cca786fb19ca67e0982267d7a06ecfa1deb9b910
BitFloyd <sby.jcub@gmail.com>
2021-01-11 15:29:56 -0500

    eval state dict and all.

--- eval.py
+++ eval.py
@@ -1,9 +1,13 @@
 import argparse
+import configparser
+import json
+import sys
 
 import torch
 from torchvision import transforms
 
 from retinanet import csv_eval
+from retinanet import model
 from retinanet.dataloader import CSVDataset, Resizer, Normalizer
 
 assert torch.__version__.split('.')[0] == '1'
@@ -17,26 +21,56 @@ def main(args=None):
     parser.add_argument('--csv', help='Path to dataset file you would like to evaluate')
     parser.add_argument('--csv_classes', help='Path to file containing class list (see readme)')
     parser.add_argument('--model_path', help='Path to the model file.')
-    parser.add_argument('--minside', help='The minside to scale the image to')
-    parser.add_argument('--maxside', help='The maxside to scale the image to')
+    parser.add_argument('--configfile', help='Path to the config file.')
 
     parser = parser.parse_args(args)
+    configs = configparser.ConfigParser()
+    configs.read(parser.configfile)
+
+    try:
+
+        depth = int(configs['TRAINING']['depth'])
+        maxside = int(configs['TRAINING']['maxside'])
+        minside = int(configs['TRAINING']['minside'])
+        try:
+            ratios = json.loads(configs['MODEL']['ratios'])
+            scales = json.loads(configs['MODEL']['scales'])
+        except Exception as e:
+            print(e)
+            print('USING DEFAULT RATIOS AND SCALES')
+            ratios = None
+            scales = None
+    except Exception as e:
+        print(e)
+        print('CONFIG FILE IS INVALID. PLEASE REFER TO THE EXAMPLE CONFIG FILE AT config.txt')
+        sys.exit()
 
     if parser.csv is None:
         dataset_eval = None
         print('No validation annotations provided.')
     else:
         dataset_eval = CSVDataset(train_file=parser.csv, class_list=parser.csv_classes,
-                                  transform=transforms.Compose([Normalizer(), Resizer(min_side=int(parser.minside),
-                                                                                      max_side=int(parser.maxside))]))
+                                  transform=transforms.Compose([Normalizer(), Resizer(min_side=minside,
+                                                                                      max_side=maxside)]))
+    # Create the model
+    if depth == 18:
+        retinanet = model.resnet18(num_classes=dataset_eval.num_classes(), pretrained=False, ratios=ratios,
+                                   scales=scales)
+    elif depth == 50:
+        retinanet = model.resnet50(num_classes=dataset_eval.num_classes(), pretrained=True, ratios=ratios,
+                                   scales=scales)
+    else:
+        print(f"DEPTH FROM : {parser.configfile} INACCURATE. MUST BE 18 or 50")
+        sys.exit(0)
 
     if torch.cuda.is_available():
-        retinanet = torch.load(parser.model_path)
         retinanet = retinanet.cuda()
         retinanet = torch.nn.DataParallel(retinanet).cuda()
+        retinanet.load_state_dict(parser.model_path)
+
     else:
-        retinanet = torch.load(parser.model_path, map_location=torch.device('cpu'))
         retinanet = torch.nn.DataParallel(retinanet)
+        retinanet.load_state_dict(torch.load(parser.model_path, map_location=torch.device('cpu')))
 
 
     retinanet.eval()
